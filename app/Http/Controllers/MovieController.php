26b288a6cd08711a487092659b227bfa574ab0f4
IonCatana <catana.ion17@yahoo.it>
2022-04-21 15:33:09 +0200

    bonus finito

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     //
+    public function index()
+    {
+        return view('home');
+    }
+    public function movie()
+    {
+        
+        $movies = Movie::all();
+
+        return view('movies', compact('movies'));
+    }
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="title">Movies</h1>
+    <div class="container">
+        <h1 class="title">Movies</h1>
+        <div class="movies">
+            @forelse ($movies as $movie)
+                <div class="movie_card">
+                    <h4>Title: {{ $movie->title }}</h4>
+                    <h5>Original Title: {{ $movie->original_title }}</h5>
+                    <div class="info">
+                        <p>Nationality: {{ $movie->nationality }}</p>
+                        <p> Date release: {{ $movie->date }}</p>
+                        <p>Vote: {{ $movie->vote }}</p>
+                    </div>
+                </div>
+
+            @empty
+                <p> No movie found</p>
+            @endforelse
+        </div>
+    </div>
 @endsection
