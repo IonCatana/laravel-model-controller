193e118659f18a9ed715c55ab675d80412c62e3d
IonCatana <catana.ion17@yahoo.it>
2022-04-21 14:44:50 +0200

    create layout and install model e controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1 class="title">Home Page</h1>
+    </div>
+@endsection
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="title">Movies</h1>
+@endsection
